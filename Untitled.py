70e175e44639e0e9abc00bc5bdd88a6e062af0bf
psingh1610 <33509378+psingh1610@users.noreply.github.com>
2020-02-10 22:20:16 -0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def create_database():
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+ 
+    # close connection to default database
+    conn.close()    
+    
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+    
+    return cur, conn
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    cur, conn = create_database()
+    
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+# In[ ]:
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+drop_tables(cur,con)
+create_tables(cur,con)
+
+
+# In[1]:
+
+
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath,lines=True)
+
+    # insert song record
+    song_data = df.iloc[:,[7,8,0,9,5]]
+    cur.execute(song_table_insert, song_data.values.tolist()[0])
+    
+    # insert artist record
+    artist_data = df.iloc[:,[0,4,2,1,3]]    
+    cur.execute(artist_table_insert, artist_data.values.tolist()[0])
+
+
+
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath,lines=True)
+
+    # filter by NextSong action
+    df = df[df['page']=='NextSong']
+
+    # convert timestamp column to datetime
+    t = 
+    
+    # insert time data records
+    time_data = 
+    column_labels = 
+    time_df = 
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = 
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = 
+        cur.execute(songplay_table_insert, songplay_data)
+
+    
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# In[4]:
+
+
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+
+
+# In[5]:
+
+
+
+artist_table_create = ("""CREATE TABLE  IF NOT EXISTS artists (
+artist_id varchar(255), name varchar(255), location varchar(255), latitude varchar(100), longitude varchar(100))
+""")
+
+
+# In[5]:
+
+
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath,lines=True)
+
+    # insert song record
+    song_data = df.iloc[:,[7,8,0,9,5]]
+    cur.execute(song_table_insert, song_data.values.tolist()[0])
+    
+    # insert artist record
+    artist_data = df.iloc[:,[0,4,2,1,3]]    
+    cur.execute(artist_table_insert, artist_data.values.tolist()[0])
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+   # process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
+
+
+
+
